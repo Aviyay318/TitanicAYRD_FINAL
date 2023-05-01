b3170f8165134173da92ae07004bb7a9663f782d
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2023-05-02 01:34:03 +0300

    chang

--- src/Constants.java
+++ src/Constants.java
@@ -1,7 +1,9 @@
 public class Constants {
     public static final String PATH_TO_DATA_FILE = "src/data/titanic.csv";
-    public static final int WINDOW_WIDTH = 600;
-    public static final int WINDOW_HEIGHT = 700;
+    public static final String [] GENDER = {"All","male","female"};
+    public static final String [] EMBARKED = {"All","charburgh","queenstown","southHampton"};
+    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_HEIGHT = 800;
     public static final String[] PASSENGER_CLASS_OPTIONS = { "All", "1st", "2nd", "3rd"};
     public static final int MARGIN_FROM_TOP = 10;
     public static final int MARGIN_FROM_LEFT = 5;
@@ -9,5 +11,51 @@ public class Constants {
     public static final int LABEL_HEIGHT = 30;
     public static final int COMBO_BOX_WIDTH = 80;
     public static final int COMBO_BOX_HEIGHT = 30;
+    public static final int J_TEXT_BOX_WIDTH = 100;
+    public static final int J_TEXT_BOX_HEIGHT = 40;
+    public static final int J_TEXT_FROM_TOP = 300;
+    public static final int J_TEXT_FROM_LEFT = 300;
+    public static final String MALE = "male";
+    public static final String FEMALE = "female";
+    public static final int SMALLER_THAN_TEN = 1;
+    public static final int BETWEEN_ELEVEN_THIRTY =2;
+    public static final int HIGHER_THAN_THIRTY=3;
+    public static final int X_LABEL = 500;
+    public static final int Y_LABEL = 400;
+    public static final int SPACE = 20;
+    public static final int X_BUTTON=270;
+    public static final int Y_BUTTON=270;
+    public static final int WIDTH_BUTTON= 100;
+    public static final int HEIGHT_BUTTON=45;
+    public static final int Y_LABEL_SPACE=30;
+    public static final int PCLASS_ONE = 1;
+    public static final int PCLASS_TWO=2;
+    public static final int PCLASS_THREE=3;
+    public static final String FIRST_CLASS= "1st";
+    public static final String SECOND_CLASS="2nd";
+    public static final String THIRD_CLASS="3rd";
+    public static final String CHARBURGH_NAME= "Charburgh";
+    public static final char CHARBURGH_CHAR= 'C';
+    public static final char QUEENSTOWN_CHAR= 'Q';
+    public static final String QUEENSTOWN_NAME= "Queenstown";
+    public static final char SOUTH_HAMPTON_CHAR= 'S';
+    public static final String SOUTH_HAMPTON_NAME= "South Hampton";
+    public static final int BETWEEN_ZERO_TEN=1;
+    public static final int BETWEEN_ELEVEN_TWENTY=2;
+    public static final int BETWEEN_TWENTY_ONE_THIRTY=3;
+    public static final int BETWEEN_THIRTY_ONE_FORTY=4;
+    public static final int BETWEEN_FORTY_ONE_FIFTY=5;
+    public static final int FIFTY_ONE_PLUS=6;
+    public static final String[] PRICES = {"Less than 10 pounds", "Between 11 to 30 pounds", "30+ pounds"};
+    public static final String[] AGES = {"Between 0 to 10", "Between 11 to 20", "Between 21 to 30", "Between 31 to 40", "Between 41 to 50", "51+"};
+    public static final String[] GENDERS= {"male", "female"};
+    public static final int NO_RELATIVES=0;
+    public static final int ONE_HUNDRED_PERCENT=100;
 
+
+//            switch (this.survivedComboBox.getItemAt(this.survivedComboBox.getSelectedIndex())){
+//                case "charburgh" -> myE = "C";
+//                case "queenstown" -> myE = "Q";
+//                case "southHampton" -> myE = "S";
+//            }
 }
