bb769f087df456a9f87bfec7b70f00453522615a
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2023-05-02 01:37:20 +0300

    Changes by Anatoly

--- src/Titanic.java
+++ src/Titanic.java
@@ -1,13 +1,21 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
-class Titanic extends JFrame {
+public class Titanic extends JFrame {
 
-    public static void main(String[] args) {
-        new Titanic();
-    }
+    private Image icon;
 
     public Titanic() {
         this.setTitle("Titanic Passengers Data");
+        try {
+            this.icon = ImageIO.read(new File("src/data/titanicIcon.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.setIconImage(this.icon);
         this.setLayout(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
